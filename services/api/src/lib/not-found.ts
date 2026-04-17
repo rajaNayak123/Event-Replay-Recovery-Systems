4f6f49a79839078f93782689bc2075cb2da140da
rajaNayak123 <nayakraja151@gmail.com>
2026-04-17 13:58:14 +0530

    middleware to handle undefined routes

---
+++ services/api/src/lib/not-found.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export function notFoundHandler(_req: Request, res: Response) {
+  return res.status(404).json({
+    message: "Route not found"
+  });
+}
