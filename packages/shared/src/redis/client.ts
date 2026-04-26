354e78ea0d928c2160f58766262168494db2d0cc
rajaNayak123 <nayakraja151@gmail.com>
2026-04-26 13:40:12 +0530

    Add error handler in redis

--- packages/shared/src/redis/client.ts
+++ packages/shared/src/redis/client.ts
@@ -1,7 +1,12 @@
 import Redis from "ioredis";
 import { env } from "../config/env";
+import { logger } from "../logging/logger";
 
 export const redis = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null,
   enableReadyCheck: true
+});
+
+redis.on("error", (error) => {
+  logger.error({ error }, "Redis connection error");
 });
