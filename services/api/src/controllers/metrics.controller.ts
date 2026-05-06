49e55fee8adb51e626e0a6535e428e821ada38ef
rajaNayak123 <nayakraja151@gmail.com>
2026-05-06 17:54:03 +0530

    add authentication to metrics endpoint

--- services/api/src/controllers/metrics.controller.ts
+++ services/api/src/controllers/metrics.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { failedEventService } from "../services/failed-event.service";
+import { verifyToken } from "../lib/auth";
 
-export async function getMetrics(_req: Request, res: Response) {
+export async function getMetrics(req: Request, res: Response) {
+  await verifyToken(req);
   const counts = await failedEventService.counts();
   return res.json({
     failedEventsByStatus: counts
